90d87ae77ccb25dd4a497a76b9e290e14e238655
yjh0410 <1394571815@qq.com>
2021-11-18 21:39:56 +0800

    update

--- train_yolotiny.sh
+++ train_yolotiny.sh
@@ -2,6 +2,7 @@ python train.py \
         --cuda \
         -v yolo_tiny \
         -d coco \
+        --root /mnt/share/ssd2/dataset \
         --batch_size 16 \
         --img_size 512 \
         --multi_scale_range 10 16 \
--- train_yolov4.sh
+++ train_yolov4.sh
@@ -1,6 +1,7 @@
 python train.py \
         --cuda \
         -d coco \
+        --root /mnt/share/ssd2/dataset \
         --batch_size 16 \
         --img_size 640 \
         --multi_scale_range 10 20 \
