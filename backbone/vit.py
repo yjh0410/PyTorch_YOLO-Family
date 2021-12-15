4b48b9a054baf853d7a96b7e87136f7d639e744a
yjh0410 <1394571815@qq.com>
2021-12-15 10:24:33 +0800

    update

--- backbone/vit.py
+++ backbone/vit.py
@@ -263,7 +263,7 @@ class PretrainVisionTransformerEncoder(nn.Module):
         self.num_classes = num_classes
         self.head = nn.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
 
-    def forward_features(self, x, mask):
+    def forward_features(self, x):
         x = self.patch_embed(x)
         
         # cls_tokens = self.cls_token.expand(batch_size, -1, -1) 
@@ -271,7 +271,7 @@ class PretrainVisionTransformerEncoder(nn.Module):
         x = x + self.pos_embed.type_as(x).to(x.device).clone().detach()
 
         B, _, C = x.shape
-        x_vis = x[~mask].reshape(B, -1, C) # ~mask means visible
+        x_vis = x.reshape(B, -1, C) # ~mask means visible
 
         for blk in self.blocks:
             x_vis = blk(x_vis)
@@ -282,66 +282,6 @@ class PretrainVisionTransformerEncoder(nn.Module):
     def forward(self, x, mask):
         x = self.forward_features(x, mask)
         x = self.head(x)
-        return x
-
-class PretrainVisionTransformerDecoder(nn.Module):
-    """ Vision Transformer with support for patch or hybrid CNN input stage
-    """
-    def __init__(self, patch_size=16, num_classes=768, embed_dim=768, depth=12,
-                 num_heads=12, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop_rate=0., attn_drop_rate=0.,
-                 drop_path_rate=0., norm_layer=nn.LayerNorm, init_values=None, num_patches=196,
-                 ):
-        super().__init__()
-        self.num_classes = num_classes
-        assert num_classes == 3 * patch_size ** 2
-        self.num_features = self.embed_dim = embed_dim  # num_features for consistency with other models
-        self.patch_size = patch_size
-
-        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]  # stochastic depth decay rule
-        self.blocks = nn.ModuleList([
-            Block(
-                dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer,
-                init_values=init_values)
-            for i in range(depth)])
-        self.norm =  norm_layer(embed_dim)
-        self.head = nn.Linear(embed_dim, num_classes) if num_classes > 0 else nn.Identity()
-
-        self.apply(self._init_weights)
-
-
-    def _init_weights(self, m):
-        if isinstance(m, nn.Linear):
-            nn.init.xavier_uniform_(m.weight)
-            if isinstance(m, nn.Linear) and m.bias is not None:
-                nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.LayerNorm):
-            nn.init.constant_(m.bias, 0)
-            nn.init.constant_(m.weight, 1.0)
-
-    def get_num_layers(self):
-        return len(self.blocks)
-
-    @torch.jit.ignore
-    def no_weight_decay(self):
-        return {'pos_embed', 'cls_token'}
-
-    def get_classifier(self):
-        return self.head
-
-    def reset_classifier(self, num_classes, global_pool=''):
-        self.num_classes = num_classes
-        self.head = nn.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
-
-    def forward(self, x, return_token_num):
-        for blk in self.blocks:
-            x = blk(x)
-
-        if return_token_num > 0:
-            x = self.head(self.norm(x[:, -return_token_num:])) # only return the mask tokens predict pixels
-        else:
-            x = self.head(self.norm(x)) # [B, N, 3*16^2]
-
         return x
 
 class PretrainVisionTransformer(nn.Module):
@@ -355,10 +295,6 @@ class PretrainVisionTransformer(nn.Module):
                  encoder_embed_dim=768, 
                  encoder_depth=12,
                  encoder_num_heads=12, 
-                 decoder_num_classes=768, 
-                 decoder_embed_dim=512, 
-                 decoder_depth=8,
-                 decoder_num_heads=8, 
                  mlp_ratio=4., 
                  qkv_bias=False, 
                  qk_scale=None, 
@@ -390,30 +326,6 @@ class PretrainVisionTransformer(nn.Module):
             init_values=init_values,
             use_learnable_pos_emb=use_learnable_pos_emb)
 
-        self.decoder = PretrainVisionTransformerDecoder(
-            patch_size=patch_size, 
-            num_patches=self.encoder.patch_embed.num_patches,
-            num_classes=decoder_num_classes, 
-            embed_dim=decoder_embed_dim, 
-            depth=decoder_depth,
-            num_heads=decoder_num_heads, 
-            mlp_ratio=mlp_ratio, 
-            qkv_bias=qkv_bias, 
-            qk_scale=qk_scale, 
-            drop_rate=drop_rate, 
-            attn_drop_rate=attn_drop_rate,
-            drop_path_rate=drop_path_rate, 
-            norm_layer=norm_layer, 
-            init_values=init_values)
-
-        self.encoder_to_decoder = nn.Linear(encoder_embed_dim, decoder_embed_dim, bias=False)
-
-        self.mask_token = nn.Parameter(torch.zeros(1, 1, decoder_embed_dim))
-
-        self.pos_embed = get_sinusoid_encoding_table(self.encoder.patch_embed.num_patches, decoder_embed_dim)
-
-        trunc_normal_(self.mask_token, std=.02)
-
 
     def _init_weights(self, m):
         if isinstance(m, nn.Linear):
@@ -434,20 +346,10 @@ class PretrainVisionTransformer(nn.Module):
     def forward(self, x, mask):
         
         x_vis = self.encoder(x, mask) # [B, N_vis, C_e]
-        x_vis = self.encoder_to_decoder(x_vis) # [B, N_vis, C_d]
 
         B, N, C = x_vis.shape
         
-        # we don't unshuffle the correct visible token order, 
-        # but shuffle the pos embedding accorddingly.
-        expand_pos_embed = self.pos_embed.expand(B, -1, -1).type_as(x).to(x.device).clone().detach()
-        pos_emd_vis = expand_pos_embed[~mask].reshape(B, -1, C)
-        pos_emd_mask = expand_pos_embed[mask].reshape(B, -1, C)
-        x_full = torch.cat([x_vis + pos_emd_vis, self.mask_token + pos_emd_mask], dim=1)
-        # notice: if N_mask==0, the shape of x is [B, N_mask, 3 * 16 * 16]
-        x = self.decoder(x_full, pos_emd_mask.shape[1]) # [B, N_mask, 3 * 16 * 16]
-
-        return x
+        return x_vis
 
         
 @register_model
@@ -459,10 +361,6 @@ def pretrain_mae_base_patch16_224(pretrained=False, **kwargs):
         encoder_depth=12, 
         encoder_num_heads=12,
         encoder_num_classes=0,
-        decoder_num_classes=768,
-        decoder_embed_dim=384,
-        decoder_depth=4,
-        decoder_num_heads=6,
         mlp_ratio=4, 
         qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6), 
