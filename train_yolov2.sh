8ed3db537e157585faadaf98d119ce81ff23b1ea
yjh0410 <1394571815@qq.com>
2022-01-04 22:50:06 +0800

    scale loss by batch

--- train_yolov2.sh
+++ train_yolov2.sh
@@ -9,6 +9,6 @@ python train.py \
         --lr_epoch 100 150 \
         --multi_scale \
         --multi_scale_range 10 20 \
-        --scale_loss pos \
+        --scale_loss batch \
         --ema
         
