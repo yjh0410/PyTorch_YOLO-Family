6e78fce38b3ffe3b9372628ad74197205c09d752
yjh0410 <1394571815@qq.com>
2022-01-22 10:47:11 +0800

    accumulate 4

--- train_yolov4_exp.sh
+++ train_yolov4_exp.sh
@@ -5,11 +5,12 @@ python train.py \
         --root /mnt/share/ssd2/dataset \
         --batch_size 16 \
         --lr 0.001 \
-        --img_size 608 \
+        --img_size 640 \
         --max_epoch 250 \
         --lr_epoch 130 180 \
         --multi_scale \
-        --multi_scale_range 10 19 \
+        --multi_scale_range 10 20 \
+        --accumulate 4 \
         --mosaic \
         --multi_anchor \
         --ema
