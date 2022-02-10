e91f3d4cc2cd2dd03f3e100a7122be6664347d59
yjh0410 <1394571815@qq.com>
2022-02-10 23:29:55 +0800

    update

--- train_yolov5.sh
+++ train_yolov5.sh
@@ -5,7 +5,7 @@ python train.py \
         --root /mnt/share/ssd2/dataset \
         --batch_size 16 \
         --lr 0.001 \
-        --img_size 640 \
+        --img_size 608 \
         --max_epoch 150 \
         --lr_epoch 90 120 \
         --multi_scale \
