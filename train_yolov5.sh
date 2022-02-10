61ca64cc8138cf62d91a14d477f053265acf00a4
yjh0410 <1394571815@qq.com>
2022-02-10 23:38:10 +0800

    update

--- train_yolov5.sh
+++ train_yolov5.sh
@@ -3,7 +3,7 @@ python train.py \
         -d coco \
         -m yolov5_l \
         --root /mnt/share/ssd2/dataset \
-        --batch_size 16 \
+        --batch_size 15 \
         --lr 0.001 \
         --img_size 640 \
         --max_epoch 150 \
@@ -11,7 +11,7 @@ python train.py \
         --multi_scale \
         --multi_scale_range 10 20 \
         --scale_loss batch \
-        --accumulate 4 \
+        --accumulate 1 \
         --mosaic \
         --multi_anchor \
         --ema
