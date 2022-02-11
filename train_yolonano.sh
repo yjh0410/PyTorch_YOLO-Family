c1f6baaba10b849af0c48f48725878b9db07af25
yjh0410 <1394571815@qq.com>
2022-02-11 14:54:15 +0800

    AdamW&Cos for YOLO-Nano

--- train_yolonano.sh
+++ train_yolonano.sh
@@ -1,13 +1,14 @@
 python train.py \
         --cuda \
         -d coco \
-        -m yolo_nano2 \
+        -m yolo_nano \
         --root /home/jxk/object-detection/dataset \
         --batch_size 64 \
         --lr 0.001 \
         --img_size 512 \
         --max_epoch 160 \
-        --lr_epoch 100 130 \
+        --optimizer adamw \
+        --lr_schedule cos \
         --multi_scale \
         --multi_scale_range 10 16 \
         --multi_anchor \
