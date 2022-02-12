00f0122b0d23bd92fcd1720b97fb2ecab3e2bf35
yjh0410 <1394571815@qq.com>
2022-02-12 19:22:06 +0800

    add yolo-nano-plus

--- train_yolonano.sh
+++ train_yolonano.sh
@@ -8,9 +8,8 @@ python train.py \
         --img_size 512 \
         --max_epoch 160 \
         --optimizer adamw \
-        --grad_clip 35 \
+        --grad_clip 1 \
         --lr_schedule cos \
-        --eval_epoch 2 \
         --multi_scale \
         --multi_scale_range 10 16 \
         --multi_anchor \
