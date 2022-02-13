b6fba81f13b670f9baddb1069448b67a73f84d44
yjh0410 <1394571815@qq.com>
2022-02-13 08:48:47 +0800

    set grad clip as 1

--- train_yolonano.sh
+++ train_yolonano.sh
@@ -10,6 +10,7 @@ python train.py \
         --optimizer adamw \
         --grad_clip 1 \
         --lr_schedule cos \
+        --eval_epoch 2 \
         --multi_scale \
         --multi_scale_range 10 16 \
         --multi_anchor \
