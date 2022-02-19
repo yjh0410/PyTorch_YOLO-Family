0e01ab85f6659df60f33f27e7718712f64680e0b
yjh0410 <1394571815@qq.com>
2022-02-19 09:22:53 +0800

    add freeze stage in yolox_backbone

--- train_yolov5.sh
+++ train_yolov5.sh
@@ -6,12 +6,12 @@ python train.py \
         --batch_size 16 \
         --lr 0.001 \
         --img_size 608 \
-        --max_epoch 200 \
-        --lr_epoch 100 150 \
+        --max_epoch 160 \
+        --lr_epoch 100 130 \
         --multi_scale \
         --multi_scale_range 10 19 \
         --scale_loss batch \
-        --accumulate 1 \
+        --accumulate 4 \
         --mosaic \
         --multi_anchor \
         --ema
