bd1b30446b4c28b57777a7c9a4b5c8ae5ffe2752
yjh0410 <1394571815@qq.com>
2022-02-20 09:10:08 +0800

    update

--- train_yolov5.sh
+++ train_yolov5.sh
@@ -2,7 +2,7 @@ python train.py \
         --cuda \
         -d coco \
         -m yolov5_l \
-        --root /mnt/share/ssd2/dataset \
+        --root /home/jxk/object-detection/dataset \
         --batch_size 16 \
         --lr 0.001 \
         --img_size 608 \
